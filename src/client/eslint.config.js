a6e06aa851d4ac807ec86daf9b124bcdef4943e8
lsommarberg <laura.sommarberg@gmail.com>
2025-01-13 12:30:49 +0200

    fixed eslint in client

--- src/client/eslint.config.js
+++ src/client/eslint.config.js
@@ -35,10 +35,6 @@ export default [
         'error',
         'single'
       ],
-      'semi': [
-        'error',
-        'never'
-      ],
       'eqeqeq': 'error',
       'no-trailing-spaces': 'error',
       'object-curly-spacing': [
