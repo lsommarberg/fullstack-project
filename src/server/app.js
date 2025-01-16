6022b3c5c14be7783a16a3b114a9d47129dc0957
lsommarberg <laura.sommarberg@gmail.com>
2025-01-16 13:55:46 +0200

    Add logger and Morgan for enhanced request logging

--- src/server/app.js
+++ src/server/app.js
@@ -2,17 +2,19 @@ const express = require('express');
 require('express-async-errors');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 
 const config = require('./utils/config');
 const { errorHandler, tokenExtractor } = require('./utils/middleware');
+const logger = require('./utils/logger');
 
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => {
-    console.log('connected to MongoDB');
+    logger.info('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message);
+    logger.info('error connection to MongoDB:', error.message);
   });
 
 const signupRouter = require('./controllers/signup');
@@ -24,6 +26,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(tokenExtractor);
+
+morgan.token('data', (req) => {
+  return req.method === 'POST' ? JSON.stringify(req.body) : '';
+});
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data'),
+);
 
 app.use('/api/signup', signupRouter);
 app.use('/api/users', usersRouter);
