bbb3c2d4e7bf116dca44159b9c432a6c91ad3dae
lsommarberg <laura.sommarberg@gmail.com>
2025-02-05 13:12:37 +0200

    add list that shows user patterns

---
+++ src/client/src/components/PatternsList.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { Stack, Text, Flex, Card } from '@chakra-ui/react';
+import { useParams, useNavigate, Link as RouterLink } from 'react-router-dom';
+import patternService from '../services/pattern';
+import { Tag } from '@/components/ui/tag';
+import SidebarLayout from './SidebarLayout';
+
+const PatternList = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [patterns, setPatterns] = useState([]);
+
+  useEffect(() => {
+    const getPatternData = async () => {
+      try {
+        const patterns = await patternService.getPatterns(id);
+        setPatterns(patterns);
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          navigate('/');
+        }
+      }
+    };
+    getPatternData();
+  }, [id, navigate]);
+
+  return (
+    <SidebarLayout userId={id}>
+      <Stack spacing={4}>
+        {patterns.map((pattern, index) => (
+          <Card.Root key={index} variant="outline">
+            <RouterLink
+              to={`/patterns/${id}/${pattern.id}`}
+              style={{ textDecoration: 'none' }}
+            >
+              <Card.Body>
+                <Flex justify="space-between" align="center">
+                  <Text fontSize="lg" fontWeight="bold">
+                    {pattern.name}
+                  </Text>
+                  <Flex>
+                    {pattern.tags.map((tag, tagIndex) => (
+                      <Tag key={tagIndex} colorScheme="blue" mr={2}>
+                        {tag}
+                      </Tag>
+                    ))}
+                  </Flex>
+                </Flex>
+              </Card.Body>
+            </RouterLink>
+          </Card.Root>
+        ))}
+      </Stack>
+    </SidebarLayout>
+  );
+};
+
+export default PatternList;
