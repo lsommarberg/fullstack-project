4ec0a8a9f4a10adec951f157aca6dda6cd311e12
lsommarberg <laura.sommarberg@gmail.com>
2025-02-24 10:28:09 +0200

    fix frontend tests

--- src/client/src/tests/patternPage.test.js
+++ src/client/src/tests/patternPage.test.js
@@ -17,7 +17,7 @@ const getMockPattern = () => ({
   name: 'Test Pattern',
   text: 'This is a test pattern.',
   link: 'http://example.com',
-  notes: ['Note 1', 'Note 2'],
+  notes: 'Note',
   tags: ['Tag 1', 'Tag 2'],
 });
 
@@ -56,35 +56,9 @@ describe('PatternPage', () => {
         ),
       ).toBeInTheDocument();
       expect(screen.getByText('Link to pattern')).toBeInTheDocument();
-      expect(screen.getByText('Note 1')).toBeInTheDocument();
-      expect(screen.getByText('Note 2')).toBeInTheDocument();
+      expect(screen.getByText('Note')).toBeInTheDocument();
       expect(screen.getByText('Tag 1')).toBeInTheDocument();
       expect(screen.getByText('Tag 2')).toBeInTheDocument();
-    });
-  });
-
-  test('handles add note', async () => {
-    patternService.getPatternById.mockResolvedValue(getMockPattern());
-    patternService.updatePattern.mockResolvedValue({
-      ...getMockPattern(),
-      notes: ['Note 1', 'Note 2', 'New Note'],
-    });
-
-    render(<PatternPage />);
-
-    await waitFor(() => {
-      expect(screen.getByText('Test Pattern')).toBeInTheDocument();
-    });
-
-    fireEvent.click(screen.getByText('Add Note'));
-    fireEvent.change(screen.getByPlaceholderText('Add a new note'), {
-      target: { value: 'New Note' },
-    });
-
-    fireEvent.click(screen.getByText(/save/i));
-
-    await waitFor(() => {
-      expect(screen.getByText('New Note')).toBeInTheDocument();
     });
   });
 
@@ -104,28 +78,6 @@ describe('PatternPage', () => {
 
     await waitFor(() => {
       expect(mockNavigate).toHaveBeenCalledWith('/patterns/1');
-    });
-  });
-
-  test('handles remove note', async () => {
-    patternService.getPatternById.mockResolvedValue(getMockPattern());
-    patternService.updatePattern.mockResolvedValue({
-      ...getMockPattern(),
-      notes: ['Note 1'],
-    });
-
-    render(<PatternPage />);
-
-    await waitFor(() => {
-      expect(screen.getByText('Test Pattern')).toBeInTheDocument();
-    });
-
-    // Get all delete buttons and click the one for 'Note 2'
-    const deleteButtons = screen.getAllByLabelText('Delete note');
-    fireEvent.click(deleteButtons[1]);
-
-    await waitFor(() => {
-      expect(screen.queryByText('Note 2')).not.toBeInTheDocument();
     });
   });
 
