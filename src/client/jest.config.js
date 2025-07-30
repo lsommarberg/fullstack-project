d2fa5d3d250784b8322f35d06513cbbefd0df0b7
lsommarberg <laura.sommarberg@gmail.com>
2025-07-30 13:15:55 +0300

    test fix for pipeline

--- src/client/jest.config.js
+++ src/client/jest.config.js
@@ -21,4 +21,13 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(axios|@chakra-ui|framer-motion)/)',
+  ],
+  extensionsToTreatAsEsm: ['.jsx'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
 };
